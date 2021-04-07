c9d4275146eabae8b4d6fafaf97dc98590ac61da
Dennis Nieuwenhuis <dennis.nieuwenhuis@iquality.nl>
2021-04-07 14:21:15 +0200

    Add changes for creating sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -7,9 +7,7 @@ module.exports = function(eleventyConfig) {
     // Copy the 'assets/' directory
     eleventyConfig.addPassthroughCopy("src/assets");
 
-    // Cop sitemap.xml and robots.txt files
-    eleventyConfig.addPassthroughCopy("src/robots.txt");
-    eleventyConfig.addPassthroughCopy("src/sitemap.xml");
+    // Copy favicons to root dist
     eleventyConfig.addPassthroughCopy({ 'src/assets/img/favicons': '/' });
 
     // Returns a list of people ordered by filename
--- src/people.md
+++ src/people.md
@@ -4,5 +4,4 @@ title: "Our people"
 metaDesc: Our People who make's it possible to accomplish its goal to become the supplier of choice for AGM related information
 description: ""
 backgroundImage: "action_3.jpg"
-permalink: "people/index.html"
 ---
